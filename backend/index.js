d6b217f1b8f5a5ffb281e22091c5a6c274dce785
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-11-16 19:43:16 +0300

    get read

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,20 @@
 
 const express =require('express')
 
+const fs =require("fs")
+
 const app =express()
 const PROT = 5000;
 app.use(express.json());
+
+// step by step  one get == read
+app.get("/read",(req, res, ) =>{
+    fs.readFile('./todo.json',(ree,data)=>{
+        const read=JSON.parse(data.toString())
+        res.status(200).json(read)
+    })
+
+})
 
 
 
