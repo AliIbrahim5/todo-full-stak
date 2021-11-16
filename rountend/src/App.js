81ac1d6b1593e94f711108a235e52e38c9afd972
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-11-17 00:19:07 +0300

    sohw

--- rountend/src/App.js
+++ rountend/src/App.js
@@ -1,12 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import axis from 'axios'
+import axios from 'axios'
+import React,{useEffect,useState}from 'react'
 
-const BACKEND_URL = "http://localhost:5000";
+
 function App() {
+const BACKEND_URL = "http://localhost:5000/read";
+    const [todo, setTodo] = useState([])
+    const rendar=async ()=>{
+      const data = await axios.get(BACKEND_URL).then((data)=>{setTodo(data.data)})
+      
+    }
+    
+   
+    useEffect(() => {
+      rendar()
+    }, [])
+
+  
   return (
     <div >
-      
+{console.log(todo)}
+      <ul>{todo.map((item)=>{
+return(
+<>
+<li>{item.name}</li>
+</>
+)
+      })}</ul>
     </div>
   );
 }
