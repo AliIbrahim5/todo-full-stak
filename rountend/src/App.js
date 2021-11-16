a8d27a658e5c9451e281d517f58ea432c9146724
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-11-16 23:06:31 +0300

    axios

---
+++ rountend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import axis from 'axios'
+
+const BACKEND_URL = "http://localhost:5000";
+function App() {
+  return (
+    <div >
+      
+    </div>
+  );
+}
+
+export default App;
